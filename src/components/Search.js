21f91436f3c8a8c9f8ecb97798faecd7a55bf7ba
James Woodhead <james_woodhead95@hotmail.co.uk>
2021-02-20 10:22:16 +0000

    amended getImages to return array of images

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,20 @@
 import React, { useState } from "react";
 import "../styles/Search.css";
+import getImages from "../requests/getImages";
 
 const Search = () => {
+    const [value, setValue] = useState();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        getImages(value);
+    }
     return (
         <>
-        <input className="search-input" type="text" />
+            <form className="search-form" onSubmit={handleSubmit}>
+                <input className="search-input" type="text" onChange={(e) => setValue(e.target.value)} />
+                <button className="search-button" type="submit">Search</button>
+            </form>
         </>
     );
 };
